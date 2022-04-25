c5b2f1c90ae1c605d7fcb14d3e07ad7f62911bae
Jamilah-Kiyingi <jamilahkiyingi@gmail.com>
2022-04-25 16:54:58 -0700

    All my pointer, array and string commands

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: input string.
+ * Return: the pointer to dest.
+ */
+char *rot13(char *str)
+{
+	char regALPHA[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char altALPHA[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; regALPHA[j] != '\0'; j++)
+		{
+			if (str[i] == regALPHA[j])
+			{
+				str[i] = altALPHA[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+All my pointer, array and string commands

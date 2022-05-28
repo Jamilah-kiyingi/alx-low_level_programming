d1d1dd31c888106496c6debeab08b1c7cf25abe4
Jamilah Kiyingi <jamilahkiyingi@gmail.com>
2022-05-28 02:58:51 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,32 +6,57 @@
  * @b: binary.
  *
  * Return: unsigned int.
+//  */
+// unsigned int binary_to_uint(const char *b)
+// {
+// 	unsigned int ui;
+// 	int len, base_two;
+
+// 	if (!b)
+// 		return (0);
+
+// 	ui = 0;
+
+// 	for (len = 0; b[len] != '\0'; len++)
+// 		;
+
+// 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+// 	{
+// 		if (b[len] != '0' && b[len] != '1')
+// 		{
+// 			return (0);
+// 		}
+
+// 		if (b[len] & 1)
+// 		{
+// 			ui += base_two;
+// 		}
+// 	}
+
+// 	return (ui);
+// }
+
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to the string of 0 and 1 chars
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int placeVal = 0;
+	int i;
 
 	if (!b)
 		return (0);
 
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+		placeVal = 2 * placeVal + (b[i] - '0');
 	}
 
-	return (ui);
+	return (placeVal);
 }
